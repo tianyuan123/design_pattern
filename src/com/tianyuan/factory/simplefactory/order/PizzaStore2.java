c585e469bdd46a3d604202ddc6f29168992ad042
suman <lifangzhang240@gmail.com>
2022-02-13 21:04:59 +0800

    simpleStaticFactory

--- src/com/tianyuan/factory/simplefactory/order/PizzaStore2.java
+++ src/com/tianyuan/factory/simplefactory/order/PizzaStore2.java
@@ -5,6 +5,8 @@ package com.tianyuan.factory.simplefactory.order;
  * @create 2022-02-13 20:49
  */
 public class PizzaStore2 {
+
+    // 简单工厂模式 也叫 静态工厂模式,将工厂方法写成静态的方法，方便调用
     public static void main(String[] args) {
         new OrderPizza2();
     }
